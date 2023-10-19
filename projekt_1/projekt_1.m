cd1efb1a108781446a41a740329ce84632c3214d
Kamil Kośnik <kamilkosnik@gmail.com>
2023-10-19 13:27:19 +0200

    Lepsze rysowanie wykresów - rozbite na niezależne w pkt. 2

--- projekt_1/projekt_1.m
+++ projekt_1/projekt_1.m
@@ -10,7 +10,11 @@ u(1, 1:11) = 0.5;
 u(1, 12:k_konc) = 1;
 y = zeros(1, k_konc);
 
-% Realizacja zadania 1
+% Punkt pracy według funkcji symulacji obiektu
+upp = 1;
+ypp = 1.7;
+
+%% Realizacja zadania 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%1
 
 for k=12:k_konc
     y(k) = symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
@@ -35,49 +39,53 @@ title('Wykres y(k)');
 hold off;
 export_fig('./pliki wynikowe/test_punktu_pracy_y(k).pdf');
 
-% Realizacja zadania 2
+%% Realizacja zadania 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 figure;
 for i=1:size(u_konc, 2)
-    u(1, 1:200) = 1; % U_pp=1
+    u(1, 1:200) = upp; % U_pp=1
     u(1, 201:k_konc) = u_konc(i);
-    y = zeros(1, k_konc);
-    
-    for k=12:k_konc
-        y(k) = symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
-    end
-
-    if zad_2_target == 'u'
-        plot(1:k_konc, u);
-    elseif zad_2_target == 'y'
-        plot(1:k_konc, y);
-    end
+    plot(1:k_konc, u);
     hold on;
 end
 
-if zad_2_target == 'u'
-    legend_list = strings([1 size(u_konc, 2)]);
-    xlabel('k');
-    ylabel('u(k)');
-    ylim([0.4 1.6]);
-    title('Wykres u(k)');
-    for i=1:size(u_konc, 2)
-        legend_text = 'u(k)=' + string(u_konc(i));
-        legend_list(i) = legend_text;
-    end
-    legend(legend_list, 'Location', 'best');
-    hold off;
-    export_fig('./pliki wynikowe/zad2_u(k).pdf');
-elseif zad_2_target == 'y'
-    legend_list = strings([1 size(u_konc, 2)]);
-    xlabel('k');
-    ylabel('y(k)');
-    title('Wykres y(k)');
-    for i=1:size(u_konc, 2)
-        legend_text = 'y(k)=' + string(u_konc(i));
-        legend_list(i) = legend_text;
-    end
-    legend(legend_list, 'Location', 'southeast');
-    hold off;
-    export_fig('./pliki wynikowe/zad2_y(k).pdf');
+legend_list = strings([1 size(u_konc, 2)]);
+xlabel('k');
+ylabel('u(k)');
+ylim([0.4 1.6]);
+title('Wykres u(k)');
+for i=1:size(u_konc, 2)
+    legend_text = 'u(k)=' + string(u_konc(i));
+    legend_list(i) = legend_text;
 end
+legend(legend_list, 'Location', 'best');
+hold off;
+export_fig('./pliki wynikowe/zad2_u(k).pdf');
+
+figure;
+for i=1:size(u_konc, 2)
+    u(1, 1:200) = upp; % U_pp=1
+    u(1, 201:k_konc) = u_konc(i);
+    for k=12:k_konc
+        y(k) = symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
+    end
+    plot(1:k_konc, y);
+    hold on;
+end
+
+legend_list = strings([1 size(u_konc, 2)]);
+xlabel('k');
+ylabel('y(k)');
+title('Wykres y(k)');
+for i=1:size(u_konc, 2)
+    legend_text = 'y(k)=' + string(u_konc(i));
+    legend_list(i) = legend_text;
+end
+legend(legend_list, 'Location', 'southeast');
+hold off;
+export_fig('./pliki wynikowe/zad2_y(k).pdf');
+
+% ch-ka stat%
+
+
+
