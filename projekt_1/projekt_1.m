1f95295c516e5729770ac1b0f5b5941f3d03894f
Mind18 <kacper.karting18@gmail.com>
2023-10-20 20:02:49 +0200

    Implemented step response of a object

--- projekt_1/projekt_1.m
+++ projekt_1/projekt_1.m
@@ -13,7 +13,7 @@ y = zeros(1, k_konc);
 % Inicjalizacja danych algorytmu DMC
 D = 200; % Horyzont dynamiki D
 s = zeros(1, D);
-skok_u = 2; % Wybrana odpowiedź skokowa do wyznaczenia wektora s;
+skok_u = 5; % Wybrana odpowiedź skokowa do wyznaczenia wektora s;
 
 % Punkt pracy według funkcji symulacji obiektu
 upp = 1;
@@ -35,7 +35,7 @@ ylabel('u(k)');
 ylim([0 1.2]);
 title('Wykres u(k)');
 hold off;
-export_fig('./pliki wynikowe/test_punktu_pracy_u(k).pdf');
+export_fig('./pliki_wynikowe/test_punktu_pracy_u(k).pdf');
 
 % Narysowanie wykresu y(k)
 figure;
@@ -45,7 +45,7 @@ xlabel('k');
 ylabel('y(k)');
 title('Wykres y(k)');
 hold off;
-export_fig('./pliki wynikowe/test_punktu_pracy_y(k).pdf');
+export_fig('./pliki_wynikowe/test_punktu_pracy_y(k).pdf');
 
 %% Realizacja zadania 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % wektory dla przechowywania danych statycznych
@@ -77,7 +77,7 @@ for i=1:size(u_konc, 2)
 end
 legend(legend_list, 'Location', 'best');
 hold off;
-export_fig('./pliki wynikowe/zad2_u(k).pdf');
+export_fig('./pliki_wynikowe/zad2_u(k).pdf');
 
 % Narysowanie wykresów y(k) odp. skokowej
 figure;
@@ -88,15 +88,20 @@ for i=1:size(u_konc, 2)
         y(k) = symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
     end
 
+    if skok_u == i
+        for k=1:D
+            % Realizacja zadania 3
+            s(k) = (y(k+200) - ypp) / (u_konc(skok_u) - upp);
+        end
+    end
+
     plot(1:k_konc, y);
     hold on;
   
     y_stat(i+1) = y(k_konc); % zapis danych dla wektora statycznego y
 end
 
-for k=1:D
-    s(k) = (y(k+199) - ypp) / u(skok_u) - upp; 
-end
+
 
 % Dodanie informacji do wygenerowanego wykresu
 legend_list = strings([1 size(u_konc, 2)]);
@@ -109,7 +114,7 @@ for i=1:size(u_konc, 2)
 end
 legend(legend_list, 'Location', 'southeast');
 hold off;
-export_fig('./pliki wynikowe/zad2_y(k).pdf');
+export_fig('./pliki_wynikowe/zad2_y(k).pdf');
 
 % ch-ka stat%
 % sortowanie i rysowanie danych
@@ -127,6 +132,14 @@ xlabel('u');
 ylabel('y(u)');
 title('Dane statyczne y(u)');
 hold off;
-export_fig('./pliki wynikowe/zad2_y(u)_stat.pdf');
+export_fig('./pliki_wynikowe/zad2_y(u)_stat.pdf');
 
-
+% Narysowanie odpowiedzi skokowej
+figure;
+plot(1:200, s);
+title_name = "Odpowiedź skokowa układu dla u_{konc}=" + ...
+    string(u_konc(skok_u));
+title(title_name);
+xlabel('k');
+ylabel('y(k)');
+export_fig('./pliki_wynikowe/odpowiedź_skokowa.pdf');
