85101317f32ae7b4858ed77549893cab31cbaac5
Mind18 <kacper.karting18@gmail.com>
2023-10-23 11:22:24 +0200

    Improved PID regulation algorithm. Functions properly

--- projekt_1/projekt_1.m
+++ projekt_1/projekt_1.m
@@ -10,8 +10,8 @@ u(1, 1:11) = 0.5; % Sygnał początkowy do zad.1
 u(1, 12:k_konc) = 1; % Sygnał końcowy do zad.2
 y = zeros(1, k_konc);
 % Ograniczenia regulatora DMC
-du_min = -1;
 du_max = 1;
+du_min = -du_max;
 u_min = 0.5;
 u_max = 1.5;
 % Wartość zadana dla regulatora DMC
@@ -154,13 +154,13 @@ export_fig('./pliki_wynikowe/odpowiedź_skokowa.pdf');
 %% Algorytm regulacji PID
 
 % Inicjalizacja zmiennych
-K_r = 5; T_p = 0.5; T_i = 1; T_d = 1; 
+K_r = 0.5; T_p = 0.5; T_i = 9; T_d = 2; 
 
 % warunki początkowe
 u = zeros(1, k_konc); y = zeros(1, k_konc);
-u(1:11)=0.5; y(1:11)=0;
-yzad(1:11)=0; yzad(12:k_konc)=1.5;
-e(1:11)=0;
+u(1:11)=upp; y(1:11)=ypp;
+yzad(1:11)=0; yzad(12:k_konc)=y_zad;
+e(1:k_konc)=0;
 
 % Współczynniki algorytmu
 r2 = (K_r * T_d) / T_p;
@@ -168,12 +168,25 @@ r1 = K_r * (T_p/(2*T_i) - 2*(T_d / T_p) - 1);
 r0 = K_r * (1 + (T_p / (2*T_i)) + (T_d/T_p));
 
 for k=12:k_konc % główna pętla symulacyjna
-% symulacja obiektu
- y(k)=symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
-% uchyb regulacji
- e(k)=yzad(k) - y(k);
-% sygnał sterujący regulatora PID
- u(k)=r2*e(k-2)+r1*e(k-1)+r0*e(k)+u(k-1);
+    % symulacja obiektu
+    y(k)=symulacja_obiektu8y_p1(u(k-10), u(k-11), y(k-1), y(k-2));
+    % uchyb regulacji
+    e(k)=yzad(k) - y(k);
+    % sygnał sterujący regulatora PID
+    u(k)=r2*e(k-2)+r1*e(k-1)+r0*e(k)+u(k-1);
+    % Ograniczenia zmiany sterowania
+    du = u(k) - u(k-1);
+    if du < du_min
+        u(k) = u(k-1) + du_min;
+    elseif du > du_max
+        u(k) = u(k-1) + du_max;
+    end
+    % Ograniczenia wartości sterowania
+    if u(k) < u_min
+        u(k) = u_min;
+    elseif u(k) > u_max
+        u(k) = u_max;
+    end
 end
 
 % Narysowanie wykresów
